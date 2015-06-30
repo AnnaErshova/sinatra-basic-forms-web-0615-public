0e5318e0c20770b666493eec4e0442872778da7e
AnnaErshova <anna.ershova@gmail.com>
2015-06-30 09:16:34 -0400

    all done

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -1,15 +1,28 @@
 class App < Sinatra::Base
   set :views, Proc.new { File.join(root, '../views')}
 
+  # show all songs
   get '/' do
     @songs = Song.all
     erb :'songs/index'
   end
 
+  # show individual page; route for showpage
   get '/songs/:id' do
     @song = Song.find(params[:id])
     erb :'songs/show'
   end
   
-end
+  get '/songs/:id/edit' do 
+    @song = Song.find(params[:id])
+    erb :'songs/edit'
+  end
 
+  # updates the song
+  patch '/songs/:id' do |id|
+    # @song = Song.find(params[:id])
+    Song.update(id, params['song'])
+    redirect "/songs/#{id}"
+  end
+
+end # end class
